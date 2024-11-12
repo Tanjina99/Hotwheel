6269198bf16d8ecca82e832ed30e1592416171cb
tanjina sultana <tanjina1997@yahoo.com>
2024-11-12 01:51:42 -0500

    just footer left to do

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaUserAstronaut } from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+// import "../styles/components/_navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src="/images/bg-1.png"
+              alt="BikeShop Logo"
+              className="logo-img"
+            />
+          </Link>
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." className="search-input" />
+          <span className="srch-btn">
+            <IoIosSearch />
+          </span>
+        </div>
+
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/new-arrivals" className="nav-link">
+              All Products
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav-link">
+              Contact Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav-link">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="cart-link">
+              <FaCartShopping size={22} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" className="user-link">
+              <FaUserAstronaut size={22} />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import HomePage from "../components/HomePage";
+import BestSeller from "../components/BestSeller";
+import BrandPage from "../components/BrandPage";
+import AdvertiseSection from "../components/AdvertiseSection";
+import SimpleBanner from "../components/SimpleBanner";
+import FeaturedServices from "../components/FeaturedServices";
+import CustomerFeedback from "../components/CustomerFeedBack";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <BestSeller />
+      <BrandPage />
+      <AdvertiseSection />
+      <SimpleBanner />
+      <FeaturedServices />
+      <CustomerFeedback />
+    </>
+  );
+};
+
+export default Home;
